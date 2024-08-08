35c72217f0df17f22f00d4627f1e30a0de201c40
MisPeluquerias <javier@mispeluquerias.com>
2024-08-08 16:11:33 +0200

    first commit

--- src/app/components/home/edit-home/edit-home.component.ts
+++ src/app/components/home/edit-home/edit-home.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditHomeService } from '../../../core/service/edit-home.service';
 import { ToastrService } from 'ngx-toastr';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edit-home',
   templateUrl: './edit-home.component.html',
-  styleUrls: ['./edit-home.component.scss']
+  styleUrls: ['./edit-home.component.scss'],
 })
 export class EditHomeComponent implements OnInit {
   salonId: number = 0;
   salonData: any = {
     id_salon: '',
     id_city: '',
+    id_province: '',
     plus_code: '',
     active: '',
     state: '',
@@ -38,55 +40,274 @@ export class EditHomeComponent implements OnInit {
     categories: '',
     city_name: '',
     city_zip_code: '',
-    city_details: ''
+    sortedHours: []
   };
 
+  provinces: any[] = [];
+  cities: any[] = [];
+  dias: any[] = [];
 
-  constructor(private route: ActivatedRoute, private editHomeService: EditHomeService,private toastr:ToastrService) {}
 
+  selectedFile: File | null = null;
+  images: any[] = [];
+  fileDescription = '';
+  fileGroup = '';
+  filePrincipal = false;
+  fileActive = true;
+  currentImageUrl: string = '';
+  currentImageAlt: string = '';
+  isImageOpen =false;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private editHomeService: EditHomeService,
+    private toastr: ToastrService,
+    private modalService: NgbModal
+  ) {}
+
+  
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
-      this.salonId = id ? +id : 0;  // Convierte el ID a número, asegurándote de que es un valor válido
-      if (this.salonId) {
-        this.getSalonData(this.salonId);
-      }
+      this.salonId = id ? +id : 0; // Convierte el ID a número, asegurándote de que es un valor válido
+      this.loadProvinces();
+      this.loadImages();
     });
   }
 
-  getSalonData(id_salon: number): void {
-  ;  // Activa la carga antes de la solicitud
-    this.editHomeService.getSalonById(id_salon).subscribe(
+  openImage(url: string, alt: string): void {
+    this.currentImageUrl = url;
+    this.currentImageAlt = alt;
+    this.isImageOpen = true;
+  }
+
+  closeImage(): void {
+    this.isImageOpen = false;
+  }
+
+  loadProvinces(): void {
+    this.editHomeService.getProvinces().subscribe(
       (response: any) => {
-        this.salonData = response.data; // Asegúrate de que response.data contiene los datos correctos
-          // Desactiva la carga una vez que los datos están cargados
-        //console.log(this.salonData);  // Imprime los datos recibidos en la consola para verificación
+        this.provinces = response.data;
+        if (this.salonId) {
+          this.getSalonData(this.salonId);
+        }
       },
-      error => {
-        console.error('Error fetching salon data:', error);
-          // Asegúrate de manejar también la carga en caso de error
+      (error) => {
+        console.error('Error fetching provinces:', error);
       }
     );
   }
-  updateSalon(): void {
-     // Activa la carga antes de la solicitud
-    this.editHomeService.updateSalon(this.salonData).subscribe(
+
+  getSalonData(id_salon: number): void {
+    this.editHomeService.getSalonById(id_salon).subscribe(
+      (response: any) => {
+        this.salonData = response.data; // Asegúrate de que response.data contiene los datos correctos
+        this.processHours(this.salonData.hours_old);
+
+        if (this.salonData.id_province) {
+          this.onProvinceChange(this.salonData.id_province, true);
+          //console.log(this.salonData);
+        }
+      },
+      (error) => {
+        console.error('Error fetching salon data:', error);
+      }
+    );
+  }
+
+  processHours(hours_old: string): void {
+    const days = hours_old.split(';').map(day => day.trim());
+    this.dias = days.map(day => {
+      const [nombre, manana, tarde] = day.split(',').map(part => part.trim());
+      return {
+        nombre,
+        mananaInicio: manana && manana !== 'Cerrado' ? manana.split(' ')[1] : '',
+        mananaFin: manana && manana !== 'Cerrado' ? manana.split(' ')[3] : '',
+        cerradoManana: !manana || manana === 'Cerrado',
+        tardeInicio: tarde && tarde !== 'Cerrado' ? tarde.split(' ')[1] : '',
+        tardeFin: tarde && tarde !== 'Cerrado' ? tarde.split(' ')[3] : '',
+        cerradoTarde: !tarde || tarde === 'Cerrado'
+      };
+    });
+  }
+
+  saveHours(): void {
+    let hasError = false;
+
+    // Validar y convertir this.dias a un formato adecuado para hours_old
+    const hours_old = this.dias.map(dia => {
+      let manana = 'Cerrado';
+      let tarde = 'Cerrado';
+
+      // Validar y formatear las horas de la mañana
+      if (!dia.cerradoManana) {
+        if (dia.mananaInicio && dia.mananaFin) {
+          manana = `De ${dia.mananaInicio} a ${dia.mananaFin}`;
+        } else {
+          this.toastr.error(`Las horas de la mañana para ${dia.nombre} no son válidas`);
+          hasError = true;
+        }
+      }
+
+      // Validar y formatear las horas de la tarde
+      if (!dia.cerradoTarde) {
+        if (dia.tardeInicio && dia.tardeFin) {
+          tarde = `De ${dia.tardeInicio} a ${dia.tardeFin}`;
+        } else {
+          this.toastr.error(`Las horas de la tarde para ${dia.nombre} no son válidas`);
+          hasError = true;
+        }
+      }
+
+      return `${dia.nombre}, ${manana}, ${tarde}`;
+    }).filter(item => item !== undefined).join('; ');
+
+    // Si hay errores, no continuar con la actualización
+    if (hasError) {
+      return;
+    }
+
+    // Llamar al servicio para actualizar los horarios en el servidor
+    this.editHomeService.updateSalonHours(this.salonId, hours_old).subscribe(
       response => {
+        this.toastr.success('Horarios actualizados con éxito');
+        this.modalService.dismissAll();
+        console.log(hours_old);
+      },
+      error => {
+        console.error('Error updating salon hours:', error);
+        this.toastr.error('Error al actualizar los horarios');
+      }
+    );
+  }
+
+
+  updateSalon(): void {
+    if (!this.salonData.id_city) {
+      this.toastr.warning("Por favor, seleccione una ciudad para actualizar los datos");
+      return;
+    }
+    console.log(this.salonData);
+
+    this.editHomeService.updateSalon(this.salonData).subscribe(
+      (response) => {
         console.log('Salon updated successfully', response);
         this.toastr.success(
           '<i class="las la-info-circle"> Cambios realizados con éxito</i>'
         );
-        console.log(this.salonData);
 
-        // Desactiva la carga una vez que los datos están cargados
-        // Puedes agregar aquí cualquier lógica adicional, como redireccionar al usuario o mostrar un mensaje de éxito
       },
-      error => {
-        console.error('Error updating salon', error); // Asegúrate de manejar también la carga en caso de error
-        this.toastr.success(
+      (error) => {
+        console.error('Error updating salon', error);
+        this.toastr.error(
           '<i class="las la-info-circle"> No se realizaron los cambios</i>'
         );
       }
     );
+  }
+
+  onProvinceChange(provinceId: number, initialLoad: boolean = false): void {
+    if (!initialLoad) {
+      this.salonData.id_city = '';
+      this.salonData.city_name = '';
+    }
+
+    this.editHomeService.getCitiesByProvince(provinceId).subscribe(
+      (response: any) => {
+        this.cities = response.data;
+        if (initialLoad && this.salonData.id_city) {
+          const selectedCity = this.cities.find(
+            (city) => city.id_city === this.salonData.id_city
+          );
+          if (selectedCity) {
+            this.salonData.city_name = selectedCity.city_name;
+          }
+        }
+      },
+      (error) => {
+        console.error('Error fetching cities:', error);
+      }
+    );
+  }
+
+  onCityChange(cityId: number): void {
+    const selectedCity = this.cities.find((city) => city.id_city === cityId);
+    if (selectedCity) {
+      this.salonData.city_name = selectedCity.city_name;
+      this.salonData.id_city = selectedCity.id_city;
+    }
+  }
+
+  toggleManana(dia: any): void {
+    if (dia.cerradoManana) {
+      dia.mananaInicio = '';
+      dia.mananaFin = '';
+    }
+  }
+
+  toggleTarde(dia: any): void {
+    if (dia.cerradoTarde) {
+      dia.tardeInicio = '';
+      dia.tardeFin = '';
+    }
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload(): void {
+    if (!this.selectedFile) {
+      this.toastr.error('<i class="las la-info-circle">Por favor, seleccione una imagen primero.</i>');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', this.selectedFile);
+    formData.append('file_name', this.selectedFile.name);
+    formData.append('file_description', this.fileDescription);
+    formData.append('file_group', this.fileGroup);
+    formData.append('file_principal', this.filePrincipal ? '1' : '0');
+    formData.append('file_active', this.fileActive ? '1' : '0');
+    formData.append('salon_id', this.salonId.toString()); // Convertir a string
+
+    this.editHomeService.uploadImage(formData).subscribe(
+      response => {
+        console.log('Image uploaded successfully', response);
+        this.loadImages(); // Recargar las imágenes después de la subida
+      },
+      error => console.error('Error uploading image', error)
+    );
+  }
+
+
+  loadImages(): void {
+    this.editHomeService.getImages(this.salonId).subscribe(
+      (response) => {
+        if (response.success) {
+          this.images = response.data;
+        } else {
+          console.error('Error fetching images', response);
+        }
+      },
+      (error) => {
+        console.error('Error fetching images', error);
+      }
+    );
+  }
+
+  deleteImage(imageId: number): void {
+
+      this.editHomeService.deleteImage(imageId).subscribe(response => {
+        console.log('Image deleted successfully', response);
+        this.loadImages(); // Recargar la lista de imágenes
+      }, error => {
+        console.error('Error deleting image', error);
+      });
+  }
+  imgPrefer(){
+
   }
 }
--- src/app/core/service/edit-home.service.ts
+++ src/app/core/service/edit-home.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { Observable } from 'rxjs';
-import { HttpClient,HttpParams } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+import { HttpClient,HttpHeaders,HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +26,59 @@ export class EditHomeService {
   }
   updateSalon(salonData: any): Observable<any> {
     return this.http.put(`${this.baseUrl}/edithome/updateSalon`, salonData);
+  }
+
+  getCitiesByProvince(id_province: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/edithome/getCitiesByProvince`, {
+      params: { id_province: id_province.toString() }
+    });
+  }
+
+  getProvinces(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/edithome/getProvinces`);
+  }
+
+
+  updateSalonHours(id: number, hours_old: string): Observable<any> {
+    const url = `${this.baseUrl}/edithome/updateSalonHours/${id}`;
+    const body = { hours_old };
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.put(url, body, { headers });
+  }
+
+  uploadImage(imageData: FormData): Observable<any> {
+    const url = `${this.baseUrl}/edithomeimages/uploadImg`;
+    return this.http.post(url, imageData).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+
+
+  // Manejo de errores
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Error del lado del servidor
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
+  getImages(salonId: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/edithomeimages/getImages`, {
+      params: { salon_id: salonId.toString() }
+    }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  deleteImage(imageId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/edithomeimages/deleteImage/${imageId}`);
   }
 }
